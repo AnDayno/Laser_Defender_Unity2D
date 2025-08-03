fcbd478b7bf92c6d606c939bc61c6721f8711b62
An <162073486+AnDayno@users.noreply.github.com>
2025-08-04 07:42:38 +0800

    Added boundaries

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     Vector2 rawInput;
+
+    [SerializeField] float paddingX;
+    [SerializeField] float paddingtop;
+    [SerializeField] float paddingBottom;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
@@ -14,12 +25,22 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        Vector3 delta = rawInput * Time.deltaTime * moveSpeed;
-        transform.position += delta;
+        Vector2 delta = rawInput * Time.deltaTime * moveSpeed;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingX, maxBounds.x - paddingX);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingtop);   
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 }
